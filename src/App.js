ea150995502fa6de6abc49c13a6c51026e83fda4
Harsh Gautam <hgautam208@gmail.com>
2021-02-06 16:16:07 +0530

    added dompurfiy to sanitize html

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import marked from 'marked';
+import DOMPurify from 'dompurify'
 class EditorComponent extends React.Component {
     constructor(props) {
         super(props);
@@ -13,13 +14,11 @@ class EditorComponent extends React.Component {
             <div className="editor-container">
                 <div className="d-flex flex-column">
                     <p className="header">Editor</p>
-                    <textarea 
-                        id="editor" 
+                    <textarea
+                        id="editor"
                         onChange={this.props.onInputChange}
                         value={this.props.input}
-                    >
-
-                    </textarea>
+                    />
                 </div>
             </div>
         );
@@ -31,21 +30,22 @@ class PreviewComponent extends React.Component {
         super(props);
         this.getMarkedDownText = this.getMarkedDownText.bind(this);
     }
-    
+
     getMarkedDownText() {
-        let rawMarkup = marked(this.props.previewText, {sanitize: true});
-        return { __html: rawMarkup};
+        let purifiedText = DOMPurify.sanitize(this.props.previewText)
+        let rawMarkup = marked(purifiedText);
+        return { __html: rawMarkup };
     }
 
     render() {
-        {console.log(this.sanitized_text)}
+        { console.log(this.sanitized_text) }
         return (
             <div>
                 <div className="d-flex flex-column">
                     <p className="header">Preview</p>
                     <p
-                        id="preview" 
-                        className="preview-container overflow-auto" 
+                        id="preview"
+                        className="preview-container overflow-auto"
                         dangerouslySetInnerHTML={this.getMarkedDownText()}
                     />
                 </div>
@@ -55,7 +55,7 @@ class PreviewComponent extends React.Component {
 }
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             input: '',
@@ -71,7 +71,7 @@ class App extends React.Component {
 ### And here's some other cool stuff:
 
 Heres some code, \`<div></div>\`, between 2 backticks.
-            
+
 \`\`\`
 // this is multi-line code:
             
@@ -80,18 +80,18 @@ if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
     return multiLineCode;
     }
 }
-\`\`\`}
-            
+\`\`\`
+
 You can also make text **bold**... whoa!
 Or _italic_.
 Or... wait for it... **_both!_**
 And feel free to go crazy ~~crossing stuff out~~.
-            
+
 There's also [links](https://www.freecodecamp.com), and
 > Block Quotes!
-            
+
 And if you want to get really crazy, even tables:
-            
+
 Wild Header | Crazy Header | Another Header?
 ------------ | ------------- | -------------
 Your content can | be here, and it | can be here....
@@ -101,11 +101,11 @@ And here. | Okay. | I think we get it.
     - Some are bulleted.
         - With different indentation levels.
             - That look like this.
-            
+
 1. And there are numbererd lists too.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
-            
+
 ![React Logo w/ Text](https://goo.gl/Umyytc)
             `,
         });
@@ -119,28 +119,28 @@ And here. | Okay. | I think we get it.
 
     render() {
         return (
-        <div className="body h-100">
-            <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="#">Simple React Markdown Previewer</a>
-        </nav>
-        <div className="container-fluid">
-            <div className="row mt-3">
-                <div className="col-sm-12 col-md-6 pl-md-5">
-                    <EditorComponent
-                        onInputChange={this.onInputChange}
-                        input={this.state.input}
-                        // handleKeyDown={this.handleKeyDown}
-                    />
-                </div>
-                <div className="col-sm-12 col-md-6 pr-md-5">
-                    <PreviewComponent
-                        previewText={this.state.input} 
-                    />
-                </div>
+            <div className="body h-100">
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand" href="#">Simple React Markdown Previewer</a>
+                </nav>
+                <div className="container-fluid">
+                    <div className="row mt-3">
+                        <div className="col-sm-12 col-md-6 pl-md-5">
+                            <EditorComponent
+                                onInputChange={this.onInputChange}
+                                input={this.state.input}
+                            // handleKeyDown={this.handleKeyDown}
+                            />
+                        </div>
+                        <div className="col-sm-12 col-md-6 pr-md-5">
+                            <PreviewComponent
+                                previewText={this.state.input}
+                            />
+                        </div>
+                    </div>
+                </div >
             </div>
-        </div >
-        </div>
-    );
+        );
     }
 }
 
